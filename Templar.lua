0ebf48331e82bb06f5f2eb3aeafaef8ec1e12fbf
LegionAC <battletowere9@gmail.com>
2025-11-26 22:32:58 +0000

    Update Templar.lua

--- Templar.lua
+++ Templar.lua
@@ -29,9 +29,7 @@ end
 
 local TeleportService = game:GetService("TeleportService")
 local HttpService = game:GetService("HttpService")
-
 local Servers = "https://games.roblox.com/v1/games/" .. game.PlaceId .. "/servers/Public?sortOrder=Asc&limit=100"
-local Server, Next = nil, nil
 
 local function ListServers(cursor)
     local Raw = game:HttpGet(Servers .. ((cursor and "&cursor=" .. cursor) or ""))
@@ -46,15 +44,40 @@ local function ListServers(cursor)
 	end
 end
 
+local smallestServer = nil
+local smallestPlayers = math.huge
+local Next = nil
+local allServers = {}
+
 repeat
-    local Servers = ListServers(Next)
-    Server = Servers.data[math.random(1, (#Servers.data / 3))]
-    Next = Servers.nextPageCursor
-until Server
+    local ServerList = ListServers(Next)
+    
+    if ServerList and ServerList.data then
+        for _, server in pairs(ServerList.data) do
+            if server.id ~= game.JobId and server.playing < server.maxPlayers then
+                table.insert(allServers, server)
+                if server.playing < smallestPlayers then
+                    smallestPlayers = server.playing
+                    smallestServer = server
+                end
+            end
+        end
+        
+        Next = ServerList.nextPageCursor
+    else
+        break
+    end
+    
+until not Next or smallestServer
 
 local function tp()
-    if Server.playing < Server.maxPlayers and Server.id ~= game.JobId then
-        TeleportService:TeleportToPlaceInstance(game.PlaceId, Server.id, game.Players.LocalPlayer)
+    if smallestServer then
+        print("Teleporting to server with " .. smallestPlayers .. " players")
+        TeleportService:TeleportToPlaceInstance(game.PlaceId, smallestServer.id, game.Players.LocalPlayer)
+    else
+        local randomServer = allServers[math.random(1, #allServers)]
+        warn("Could not find lowest player server. Joining random server...")
+        TeleportService:TeleportToPlaceInstance(game.PlaceId, randomServer.id, game.Players.LocalPlayer)
     end
 end
 
@@ -1337,7 +1360,7 @@ repeat
     end
 until success
 
-Network:InvokeServer("Respawn", "Bronze")
+Network:InvokeServer("Respawn", spawnLocation)
 end
 
 local function applyButtonFunctionality()
@@ -1579,7 +1602,7 @@ end
 if isTweenFinished == true then
     applyButtonFunctionality()
 else 
-    while isTwenFinished == false do
+    while isTweenFinished == false do
         wait(0.1)
     end
     applyButtonFunctionality()
